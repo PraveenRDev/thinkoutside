c26abd6d5522698399802356b7a58e10d879495a
praveenrdev <praveenr@ceylonsolutions.com>
2020-04-08 00:35:31 +0530

    version 2.0 upgrade with flexboxes and whole new design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react'
-import Header from './components/header'
+import React, {useState} from 'react'
+import { Route } from 'react-router-dom'
 
+import About from './pages/about'
+import Navigation from './components/navigation'
 const App = () => {
 
+    const [lang, setLang] = useState('EN')
+
     return(
-        <Header/>
+        <div>
+            <React.Fragment>
+                <Route exact path='/' render={() => <About lang={lang}/>}></Route>
+            </React.Fragment>
+            <Navigation lang={lang} handleLanguage={() => setLang(lang==='EN' ? 'FR' : 'EN')}/>
+       </div>
     )
 
 }
--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,9 +1,52 @@
-import React from 'react';
-import { withRouter } from 'react';
+import React, { useState, useEffect } from 'react';
+import { withRouter, Link } from 'react-router-dom';
 
-const Navigation = () => {
+import { NAVIGATION } from './../../libs/constants'
+// import { NAVIGATION_CONTENT } from './../../libs/content'
 
+import './style.scss'
 
+const Navigation = ({location, lang='EN', handleLanguage}) => {
+
+    const [page, setPage] = useState(NAVIGATION.ABOUT_ME)
+
+    useEffect(() => {
+        const { pathname } = location
+        if(pathname === '/'){
+            setPage(NAVIGATION.ABOUT_ME)
+        } else if(pathname === '/skills'){
+            setPage(NAVIGATION.SKILLS)
+        } else if(pathname === '/showcase'){
+            setPage(NAVIGATION.SHOWCASE)
+        } else if(pathname === '/contact'){
+            setPage(NAVIGATION.CONTACT)
+        }
+    }, [location])
+    
+    return(
+        <nav className='navigation-wrapper'>
+            <div className='navigation-group'>
+                <div onClick={handleLanguage} className='navigation-icon'>{lang === 'EN' ? 'FR' : 'EN'}</div>
+                {/* <div className='navigation-text'>{NAVIGATION_CONTENT[lang].LANGUAGE}</div> */}
+            </div>
+            <div className='navigation-group'>
+                <Link to='/' className={`navigation-icon ${page === NAVIGATION.ABOUT_ME ? 'highlight' : ''}`}><ion-icon name="person-circle"></ion-icon></Link>
+                {/* <Link to='/'>{NAVIGATION_CONTENT[lang].ME}</Link> */}
+            </div>
+            <div className='navigation-group'>
+                <Link to='/skills' className={`navigation-icon ${page === NAVIGATION.SKILLS ? 'highlight' : ''}`}><ion-icon name="trophy"></ion-icon></Link>
+                {/* <Link to='/skills'>{NAVIGATION_CONTENT[lang].SKILLS}</Link> */}
+            </div>
+            <div  className='navigation-group'>
+                <Link to='/showcase' className={`navigation-icon ${page === NAVIGATION.SHOWCASE ? 'highlight' : ''}`}><ion-icon name="library"></ion-icon></Link>
+                {/* <Link to='/showcase'>{NAVIGATION_CONTENT[lang].SHOWCASE}</Link> */}
+            </div>
+            <div  className='navigation-group'>
+                <Link to='/contact' className={`navigation-icon ${page === NAVIGATION.CONTACT ? 'highlight' : ''}`}><ion-icon name="chatbubbles"></ion-icon></Link>
+                {/* <Link to='/contact'>{NAVIGATION_CONTENT[lang].CONTACT}</Link> */}
+            </div>
+        </nav>
+    )
 }
 
-export default Navigation;
+export default withRouter(Navigation);
