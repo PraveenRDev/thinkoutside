2f290fd5fc72630f440688fadcc6702e2b112c4e
praveenrdev <praveenr@ceylonsolutions.com>
2020-04-11 22:03:05 +0530

    logo and favicon added

--- src/components/landing/index.js
+++ src/components/landing/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
-
 import './style.scss';
 const Landing = ({title, subTitle}) => (
     <header className='landing'>
+        {/* <img src={logo_iso}></img> */}
+        {/* <img src={logo_default}></img> */}
         <div className='landing__image-wrapper'><div className='landing__image'></div></div>
         <div className='landing__content-container'>
               <h1 className='landing__content-title'>{title}</h1>
