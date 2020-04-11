bfe828b721f54685de2cbc7d7f16d5a33cc66cb0
praveenrdev <praveenr@ceylonsolutions.com>
2020-04-11 22:28:43 +0530

    logo to screen

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -25,25 +25,28 @@ const Navigation = ({location, lang='EN', handleLanguage}) => {
     
     return(
         <nav className='navigation-wrapper'>
-            <div className='navigation-group'>
-                <div onClick={handleLanguage} className='navigation-icon lang'>{lang === 'EN' ? 'FR' : 'EN'}</div>
-                {/* <div className='navigation-text'>{NAVIGATION_CONTENT[lang].LANGUAGE}</div> */}
-            </div>
-            <div className='navigation-group'>
-                <Link to='/' className={`navigation-icon ${page === NAVIGATION.ABOUT_ME ? 'highlight' : ''}`}><ion-icon name="person-circle"></ion-icon></Link>
-                <Link to='/' className={`navigation-text ${page === NAVIGATION.ABOUT_ME ? 'highlight' : ''}`}>{NAVIGATION_CONTENT[lang].ME}</Link>
-            </div>
-            <div className='navigation-group'>
-                <Link to='/skills' className={`navigation-icon ${page === NAVIGATION.SKILLS ? 'highlight' : ''}`}><ion-icon name="trophy"></ion-icon></Link>
-                <Link to='/skills' className={`navigation-text ${page === NAVIGATION.SKILLS ? 'highlight' : ''}`}>{NAVIGATION_CONTENT[lang].SKILLS}</Link>
-            </div>
-            <div  className='navigation-group'>
-                <Link to='/showcase' className={`navigation-icon ${page === NAVIGATION.SHOWCASE ? 'highlight' : ''}`}><ion-icon name="library"></ion-icon></Link>
-                <Link to='/showcase' className={`navigation-text ${page === NAVIGATION.SHOWCASE ? 'highlight' : ''}`}>{NAVIGATION_CONTENT[lang].SHOWCASE}</Link>
-            </div>
-            <div  className='navigation-group'>
-                <Link to='/contact' className={`navigation-icon ${page === NAVIGATION.CONTACT ? 'highlight' : ''}`}><ion-icon name="chatbubbles"></ion-icon></Link>
-                <Link to='/contact' className={`navigation-text ${page === NAVIGATION.CONTACT ? 'highlight' : ''}`}>{NAVIGATION_CONTENT[lang].CONTACT}</Link>
+            <div className='navigation-logo'></div>
+            <div className='navigation-content'> 
+                <div className='navigation-group'>
+                    <div onClick={handleLanguage} className='navigation-icon lang'>{lang === 'EN' ? 'FR' : 'EN'}</div>
+                    {/* <div className='navigation-text'>{NAVIGATION_CONTENT[lang].LANGUAGE}</div> */}
+                </div>
+                <div className='navigation-group'>
+                    <Link to='/' className={`navigation-icon ${page === NAVIGATION.ABOUT_ME ? 'highlight' : ''}`}><ion-icon name="person-circle"></ion-icon></Link>
+                    <Link to='/' className={`navigation-text ${page === NAVIGATION.ABOUT_ME ? 'highlight' : ''}`}>{NAVIGATION_CONTENT[lang].ME}</Link>
+                </div>
+                <div className='navigation-group'>
+                    <Link to='/skills' className={`navigation-icon ${page === NAVIGATION.SKILLS ? 'highlight' : ''}`}><ion-icon name="trophy"></ion-icon></Link>
+                    <Link to='/skills' className={`navigation-text ${page === NAVIGATION.SKILLS ? 'highlight' : ''}`}>{NAVIGATION_CONTENT[lang].SKILLS}</Link>
+                </div>
+                <div  className='navigation-group'>
+                    <Link to='/showcase' className={`navigation-icon ${page === NAVIGATION.SHOWCASE ? 'highlight' : ''}`}><ion-icon name="library"></ion-icon></Link>
+                    <Link to='/showcase' className={`navigation-text ${page === NAVIGATION.SHOWCASE ? 'highlight' : ''}`}>{NAVIGATION_CONTENT[lang].SHOWCASE}</Link>
+                </div>
+                <div  className='navigation-group'>
+                    <Link to='/contact' className={`navigation-icon ${page === NAVIGATION.CONTACT ? 'highlight' : ''}`}><ion-icon name="chatbubbles"></ion-icon></Link>
+                    <Link to='/contact' className={`navigation-text ${page === NAVIGATION.CONTACT ? 'highlight' : ''}`}>{NAVIGATION_CONTENT[lang].CONTACT}</Link>
+                </div>
             </div>
         </nav>
     )
